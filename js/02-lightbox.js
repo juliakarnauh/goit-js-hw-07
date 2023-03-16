b16ed805611d196db0791e0016d3d2a3358fbc1c
juliakarnaukh <juliakarnayh04@gmail.com>
2023-03-16 22:55:24 +0300

    delete notes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -20,33 +20,9 @@ const structure = galleryItems.reduce((acc, element) => {
 
 listGallery.insertAdjacentHTML("afterbegin", structure);
 
-// listGallery.addEventListener("click", (event) => {
-//   event.preventDefault();
+
   let gallery = new SimpleLightbox(".gallery a",{
     captionsData: "alt",
     captionDelay: 250,
    
 });  
-// });
-
-
-// ;})
-
-// gallery.on('error.simplelightbox', function (e) {
-// 	console.log(e); // some usefull information
-// });
-// const lightbox = new SimpleLightbox(listGallery, {
-//   captionDelay: 250
-
-// let gallery = new SimpleLightbox('.gallery');
-// gallery.on('show.simplelightbox', function () {
-//     // do something…
-// });})
-
-// const lightbox = new SimpleLightbox({
-//     captionDelay: 250,
-//     captions: function(event) {
-//       return event.getAttribute('alt');
-//     }
-//   });
-//   lightbox.load(gallery.children);})
